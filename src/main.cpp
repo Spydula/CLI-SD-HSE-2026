68d9edfdbc9f385be4ceb46197011d89c472674c
Georgii Gubanov <g.gubanov@vkteam.ru>
2026-01-31 15:42:49 +0300

    init

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "shell.hpp"
+
+int main() {
+  Shell sh;
+  return sh.run();
+}
---
+++ src/shell.cpp
@@ -0,0 +1,24 @@
+#include "shell.hpp"
+#include <iostream>
+#include <string>
+
+int Shell::run() {
+  std::string line;
+
+  while (true) {
+    std::cout << "> " << std::flush;
+
+    if (!std::getline(std::cin, line)) {
+      std::cout << "\n";
+      return 0;
+    }
+
+    if (line == "exit") {
+      return 0;
+    }
+
+    if (!line.empty()) {
+      std::cout << "you typed: " << line << "\n";
+    }
+  }
+}
